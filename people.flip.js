046e2393fd0afd34e3abde47066da0f349e36836
WESSELKRAMER <info@wesselkramer.nl>
2026-05-11 13:19:02 +0200

    Update people.flip.js

--- people.flip.js
+++ people.flip.js
@@ -4,29 +4,28 @@ function initPersonalCutoutToGrid() {
   gsap.registerPlugin(ScrollTrigger);
 
   const hero = document.querySelector(".section_bigtext_hero");
-  const items = gsap.utils.toArray(".personal_cutout_wrapper");
-  const targets = gsap.utils.toArray(".personal_cutout_target");
+  const items = gsap.utils.toArray(".personal_cutout_wrapper[data-cutout]");
+  const targets = gsap.utils.toArray(".personal_cutout_target[data-target]");
 
-  if (!hero || !items.length || targets.length < items.length) return;
+  if (!hero || !items.length || !targets.length) return;
+
+  const targetMap = new Map();
+
+  targets.forEach((target) => {
+    const name = target.getAttribute("data-target");
+    if (name) targetMap.set(name, target);
+  });
 
   function getDelta(item, target) {
     const itemRect = item.getBoundingClientRect();
     const targetRect = target.getBoundingClientRect();
 
     return {
-      x:
-        targetRect.left +
-        targetRect.width / 2 -
-        (itemRect.left + itemRect.width / 2),
-
-      y:
-        targetRect.top +
-        targetRect.height / 2 -
-        (itemRect.top + itemRect.height / 2),
+      x: targetRect.left + targetRect.width / 2 - (itemRect.left + itemRect.width / 2),
+      y: targetRect.top + targetRect.height / 2 - (itemRect.top + itemRect.height / 2)
     };
   }
 
-  // disable hover initially
   items.forEach((item) => {
     item.classList.remove("is-hover-active");
   });
@@ -45,20 +44,25 @@ function initPersonalCutoutToGrid() {
         items.forEach((item) => {
           item.classList.toggle("is-hover-active", active);
         });
-      },
-    },
+      }
+    }
   });
 
-  items.forEach((item, i) => {
+  items.forEach((item) => {
+    const name = item.getAttribute("data-cutout");
+    const target = targetMap.get(name);
+
+    if (!target) return;
+
     tl.to(
       item,
       {
-        x: () => getDelta(item, targets[i]).x,
-        y: () => getDelta(item, targets[i]).y,
+        x: () => getDelta(item, target).x,
+        y: () => getDelta(item, target).y,
         rotation: 0,
         scale: 0.9,
         opacity: 1,
-        ease: "none",
+        ease: "none"
       },
       0
     );
